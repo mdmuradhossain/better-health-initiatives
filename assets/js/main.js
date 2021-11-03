ed389d5444204450b1a574910b36483e33ef1d3f
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-11-03 21:47:45 +0600

    complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,30 +41,6 @@ function scrollHeader() {
 }
 window.addEventListener("scroll", scrollHeader);
 
-// Show Active Link while Scrolling Sections
-// const sections = document.querySelectorAll("section[id]");
-
-// function scrollActive() {
-//   const scrollY = window.pageYOffset;
-
-//   sections.forEach((current) => {
-//     const sectionHeight = current.offsetHeight;
-//     const sectionTop = current.offsetTop - 50;
-//     sectionId = current.getAttribute("id");
-
-//     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-//       document
-//         .querySelector(".nav__menu a[href*=" + sectionId + "]")
-//         .classList.add("active-link");
-//     } else {
-//       document
-//         .querySelector(".nav__menu a[href*=" + sectionId + "]")
-//         .classList.remove("active-link");
-//     }
-//   });
-// }
-// window.addEventListener("scroll", scrollActive);
-
 // Scroll Top
 function scrollUp() {
   const scrollUp = document.getElementById("scroll-up");
